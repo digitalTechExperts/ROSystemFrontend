acada9daea99dfa5c509ad6667927f021cfabf86
Yaman Kanojia <digitalTechExperts07@gmail.com>
2023-09-18 19:50:02 +0530

    basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'complaints',
     loadChildren: () => import('./pages/complaints/complaints.module').then( m => m.ComplaintsPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
